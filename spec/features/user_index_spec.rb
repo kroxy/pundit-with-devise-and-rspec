d839c47961c34f180ddc26908a8dc31d676a0a3b
Tibor Kreiter <kreiter.tibor@gmail.com>
2016-02-20 19:27:09 +0100

    user index page is set up

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
  feature 'User index page ', :devise do
    scenario 'user sees own email address' do
-     user = FactoryGirl.create(:user,:adnin)
+     user = FactoryGirl.create(:user, :admin)
      login_as(user, scope: :user)
-     vit user_path
+     visit users_path
      expect(page).to have_content user.email
    end
  end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.include FactoryGirl::Syntax::Methods
   config.include Features::SessionHelpers, type: :feature
 
@@ -55,11 +55,16 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
-  #config.infer_spec_type_from_file_location!
+  config.include Warden::Test::Helpers
+  config.before :suite do
+    Warden.test_mode!
+  end
+
+
+  config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
-  #config.filter_rails_from_backtrace!
+  config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
 end
