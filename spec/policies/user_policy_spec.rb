ed5a8746aba67621c77d56f3becbcab0acd2fcbd
Tibor Kreiter <kreiter.tibor@gmail.com>
2016-02-21 20:55:37 +0100

    add destroy and update policy

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,32 +1,48 @@
 require 'rails_helper'
 
-describe UserPolicy do
-  subject { UserPolicy }
+  describe UserPolicy do
+    subject { UserPolicy }
 
-  let (:current_user){FactoryGirl.build_stubbed :user}
-  let (:other_user){FactoryGirl.build_stubbed :user}
-  let (:admin){FactoryGirl.build_stubbed :user,:admin}
+    let (:current_user){FactoryGirl.build_stubbed :user}
+    let (:other_user){FactoryGirl.build_stubbed :user}
+    let (:admin){FactoryGirl.build_stubbed :user,:admin}
 
-  permissions :index? do
-    it 'denies access if not an admin' do
-      expect(subject).not_to permit(current_user)
+    permissions :index? do
+      it 'denies access if not an admin' do
+        expect(subject).not_to permit(current_user)
+      end
+      it 'allows access for an admin' do
+        expect(subject).to permit(admin)
+      end
     end
-    it 'allows access for an admin' do
-      expect(subject).to permit(admin)
+
+    permissions :show? do
+      it 'prevents other users from seeing the profile' do
+        expect(subject).not_to permit(current_user, other_user)
+      end
+      it 'allows owner to see profile' do
+        expect(subject).to permit(current_user, current_user)
+      end
+      it 'allows and adminsee profile' do
+        expect(subject).to permit(admin)
+      end
+    end
+
+    permissions :update? do
+      it 'prevents updates if not an admin' do
+        expect(subject).not_to permit(current_user)
+      end
+      it 'allows an admin to make update' do
+        expect(subject).to permit(admin)
+      end
+    end
+
+    permissions :destroy? do
+      it 'prevents deleting yourself' do
+        expect(subject).not_to permit(current_user,current_user)
+      end
+      it 'allows an admin delete any user' do
+        expect(subject).to permit(admin,other_user)
+      end
     end
   end
-  permissions :show? do
-    it 'prevents other users from seeing the profile' do
-      expect(subject).not_to permit(current_user, other_user)
-    end
-
-    it 'allows owner to see profile' do
-      expect(subject).to permit(current_user, current_user)
-    end
-
-    it 'allows and adminsee profile' do
-      expect(subject).to permit(admin)
-
-    end
-  end
-end
