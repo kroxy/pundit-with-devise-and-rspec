d888cc020fa9bccc7f56713bed731ab888b745e5
Tibor Kreiter <kreiter.tibor@gmail.com>
2016-02-21 20:43:45 +0100

    adds show policy test

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -9,10 +9,24 @@ describe UserPolicy do
 
   permissions :index? do
     it 'denies access if not an admin' do
-      expect(UserPolicy).not_to permit(current_user)
+      expect(subject).not_to permit(current_user)
     end
     it 'allows access for an admin' do
-      expect(UserPolicy).to permit(admin)
+      expect(subject).to permit(admin)
+    end
+  end
+  permissions :show? do
+    it 'prevents other users from seeing the profile' do
+      expect(subject).not_to permit(current_user, other_user)
+    end
+
+    it 'allows owner to see profile' do
+      expect(subject).to permit(current_user, current_user)
+    end
+
+    it 'allows and adminsee profile' do
+      expect(subject).to permit(admin)
+
     end
   end
 end
