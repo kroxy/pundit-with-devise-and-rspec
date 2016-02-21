67e1fa04bc14114b14a199ae07884319f22ab52d
Tibor Kreiter <kreiter.tibor@gmail.com>
2016-02-21 20:30:50 +0100

    passing policy index

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'capybara/rails'
 require 'shoulda/matchers'
 require 'database_cleaner'
 require 'pundit/rspec'
+
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
